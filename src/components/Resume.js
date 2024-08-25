9902dafd8157dafa629d8aa731d6bd5d6b24a0d4
RajRathinam <rajrathinam2005@gmail.com>
2024-08-25 06:32:52 -0700

    fifth push

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,7 +11,7 @@ const config ={
 <section className='flex flex-col md:flex-row bg-primary p-5' id='resume'>
     <div className='md:w-1/2 flex flex-col justify-center pl-5 text-white'>
       <div className='flex flex-col justify-center'>  <h1 className='text-4xl  border-b-4 border-[#9006ec] pb-2 mb-5 w-[170px] font-bold'>Resume</h1>
-      <p className='pb-5'>You can view myresume and <a className='btn' href={config.link}>Download it </a></p>
+      <p className='pb-5'>You can view & <a className='bg-secondary px-2 py-1 font-bold hover:border-2 border-white  mx-2 rounded md:btn' href={config.link}>Download it</a></p>
 
       </div>
         
